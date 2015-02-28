c91da395f3c9eb8308a2fe76210ba5707d417e97
Szekely Zolta <zzolta@gmail.com>
2015-02-28 09:23:33 +0200

    Added search icon to the action bar.

--- app/src/main/java/com/zzolta/android/glutenfreerecipes/MainActivity.java
+++ app/src/main/java/com/zzolta/android/glutenfreerecipes/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         final int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
