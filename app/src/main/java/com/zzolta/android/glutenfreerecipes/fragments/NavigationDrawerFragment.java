ee0d9306accb129f03b827652e877180e61447fb
Szekely Zolta <zzolta@gmail.com>
2015-03-01 11:05:21 +0200

    Removed Example action.

--- app/src/main/java/com/zzolta/android/glutenfreerecipes/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/zzolta/android/glutenfreerecipes/fragments/NavigationDrawerFragment.java
@@ -15,7 +15,6 @@ import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 import com.zzolta.android.glutenfreerecipes.R;
 
 /**
@@ -233,11 +232,6 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
